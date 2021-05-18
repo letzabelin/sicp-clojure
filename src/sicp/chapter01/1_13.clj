e48205ed4b028e10670d74355a797f3e28983e6e
Andrew Zabelin <letzabelin@gmail.com>
2021-05-18 12:20:49 +0300

    done 1.13

--- src/sicp/chapter01/1_13.clj
+++ src/sicp/chapter01/1_13.clj
@@ -1,4 +1,20 @@
 (ns sicp.chapter01.1-13)
 
-(defn solution [n]
-  n)
+(defn fib [n]
+  (loop [first 0 second 1 count n]
+    (if (zero? count)
+      first
+      (recur (+ first second)
+             first
+             (dec count)))))
+
+(defn f [n]
+  (let [five-sqrt (Math/sqrt 5.0)]
+    (/ (-
+        (Math/pow (/ (+ 1 five-sqrt)
+                     2)
+                  n)
+        (Math/pow (/ (- 1 five-sqrt)
+                     2)
+                  n))
+       five-sqrt)))
--- test/sicp/chapter01/1_13_test.clj
+++ test/sicp/chapter01/1_13_test.clj
@@ -1,6 +1,9 @@
 (ns sicp.chapter01.1-13-test
   (:require [clojure.test :refer [deftest is]]
-            [sicp.chapter01.1-13 :refer [solution]]))
+            [sicp.chapter01.1-13 :refer [f fib]]))
 
 (deftest test-solution
-  (is (= 1 (solution 1))))
+  (is (= (double (fib 1)) (Math/floor (f 1))))
+  (is (= (double (fib 3)) (Math/floor (f 3))))
+  (is (= (double (fib 8)) (Math/floor (f 8))))
+  (is (= (double (fib 11)) (Math/floor (f 11)))))
