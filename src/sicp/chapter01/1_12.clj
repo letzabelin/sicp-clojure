bf6de1e664087c1a1373b3ca209a3ed88a80a152
Andrew Zabelin <a.zabelin@i-t-p.pro>
2021-05-18 11:12:42 +0300

    refactor code

--- src/sicp/chapter01/1_12.clj
+++ src/sicp/chapter01/1_12.clj
@@ -2,8 +2,13 @@
 
 (defn get-pascal-triangle-element [row-index col-index]
   (cond
-    (or (neg? row-index) (neg? col-index) (>= col-index (inc row-index))) nil
-    (or (zero? col-index) (= col-index row-index)) 1
+    (or (neg? row-index)
+        (neg? col-index)
+        (< row-index col-index)) nil
+    (or (zero? col-index)
+        (= col-index row-index)) 1
     :else (+
-           (get-pascal-triangle-element (dec row-index) (dec col-index))
-           (get-pascal-triangle-element (dec row-index) col-index))))
+           (get-pascal-triangle-element (dec row-index)
+                                        (dec col-index))
+           (get-pascal-triangle-element (dec row-index)
+                                        col-index))))
