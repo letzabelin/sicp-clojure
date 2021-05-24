f2ee81c79066416c1bb0a26a05447e57d7ef56b2
Andrew Zabelin <letzabelin@gmail.com>
2021-05-24 10:59:02 +0300

    reformat code

--- src/sicp/chapter01/1_16.clj
+++ src/sicp/chapter01/1_16.clj
@@ -2,12 +2,14 @@
   (:require [sicp.helpers :refer [square]]))
 
 (defn fast-expt-recursive [base power]
-  (cond (zero? power) 1
-        (even? power) (square (fast-expt-recursive base (/ power 2)))
-        :else (* base (fast-expt-recursive base (dec power)))))
+  (cond
+    (zero? power) 1
+    (even? power) (square (fast-expt-recursive base (/ power 2)))
+    :else (* base (fast-expt-recursive base (dec power)))))
 
 (defn fast-expt-iterative [base power]
   (loop [acc 1 b base p power]
-    (cond (zero? p) acc
-          (even? p) (recur acc (square b) (/ p 2))
-          :else (recur (* acc b) b (dec p)))))
+    (cond
+      (zero? p) acc
+      (even? p) (recur acc (square b) (/ p 2))
+      :else (recur (* acc b) b (dec p)))))
